f6b664a261559cadcc78e979ad9deb882be9400d
Xuer04 <zjajzcx3566@163.com>
2022-09-01 16:32:51 +0800

    update lua config (auto formatting when save file)

--- nvim/lua/after/plugin/null-ls.rc.lua
+++ nvim/lua/after/plugin/null-ls.rc.lua
@@ -18,7 +18,22 @@ local sources = {
   formatting.markdownlint,
 }
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
     sources = sources,
+    -- you can reuse a shared lspconfig on_attach callback here
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                    vim.lsp.buf.formatting()
+                end,
+            })
+        end
+    end,
 })
 
